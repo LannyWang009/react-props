5282059ad2c78de5ad038e8d5aeed252af8cf529
LannyWang009 <09legend.wang@gmail.com>
2019-02-16 10:44:49 -0600

    solved rectangle as an example

--- circles/App.js
+++ circles/App.js
@@ -1,9 +1,24 @@
 // Render the circles using React!
 const App = (props) => {
-    let { circles } = props;
-    return (
-        <span>circles</span>
-    )
+  let { circles } = props
+  console.log('App', props)
+  <Circle radius={props.radius} color={props.color}><span>circle</span></Circle>
 }
 
-const Circle = null;  // CHALLENGE: Write a separate Circle component for use in the App component
+const Circle = (props) => {
+    let { circles } = props
+  console.log('Circle', props)
+  props.map(element => {
+      let styles ={
+          margin:'10px',
+          width:element.radius,
+          height:element.radius,
+          backgroundColor:props.color
+      }
+      console.log('styles', styles)
+      return (
+          <div className='circle' style={styles}>             
+          </div>
+      )
+  }
+}
--- rectangle/App.js
+++ rectangle/App.js
@@ -1,7 +1,13 @@
 // Render the rectangle using React!
 const App = (props) => {
-    let { rectangle } = props;
-    return (
-        <span>rectangle</span>
-    )
+  let { rectangle } = props
+  return (
+    <Rectangle width={rectangle.width} height={rectangle.height} color={rectangle.color} />
+  )
+}
+
+const Rectangle = ({ width, height, color }) => {
+  return (
+    <div style={{ height: height, width: width, backgroundColor: color }} />
+  )
 }
